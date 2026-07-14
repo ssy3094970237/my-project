562ac2dee99adb8562f1b36239a1e24ba4dd1579
ssy3094970237 <3094970237@qq.com>
2026-07-14 17:45:18 +0800

    feat: 初始化微信小程序项目结构与全局配置

---
+++ 猫咪购物园/app.js
@@ -0,0 +1,21 @@
+// app.js
+App({
+  onLaunch() {
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+  },
+  globalData: {
+    userInfo: null,
+    goodsData:[],
+    allOrder: [] // 新增：全局订单池，购物车结算后存入，订单页读取
+  }
+})
